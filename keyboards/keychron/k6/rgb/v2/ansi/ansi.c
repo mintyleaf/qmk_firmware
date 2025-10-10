5d5bab67a36374a9cc23d5336598b04badd6ee30
mintyleaf <mintyleafdev@gmail.com>
2025-10-10 10:41:06 +0400

    fix layer highlight with default inactive leds themes

--- keyboards/keychron/k6/rgb/v2/ansi/ansi.c
+++ keyboards/keychron/k6/rgb/v2/ansi/ansi.c
@@ -177,9 +177,13 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
             for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
                 uint8_t index = g_led_config.matrix_co[row][col];
 
-                if (index >= led_min && index < led_max && index != NO_LED &&
-                    keymap_key_to_keycode(layer, (keypos_t){col, row}) > KC_TRNS) {
-                    rgb_matrix_set_color(index, RGB_WHITE);
+                if (index >= led_min && index < led_max && index != NO_LED) {
+                    if (keymap_key_to_keycode(layer, (keypos_t){col, row}) > KC_TRNS) {
+                        rgb_matrix_set_color(index, RGB_WHITE);
+                    } else {
+                        // fix non-white highlight at some rows when leds is inactive by default
+                        rgb_matrix_set_color(index, 0x01, 0x01, 0x01);
+                    }
                 }
             }
         }
